a530567154b81a319eb883c7fc8b116e21d42e27
Neto test <lyudmila.volkova.70@mail.ru>
2023-04-07 21:38:06 +0800

    first

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7_999_999_99_99";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+
+        System.out.println(post.name);
+
+    }
+}
